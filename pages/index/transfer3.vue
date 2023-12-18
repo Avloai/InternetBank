fa6c7ed6c71d573b1b617bb51bfccdc4c5f95b47
avloai <naciyue@gmail.com>
2023-12-18 22:26:56 +0800

    账号转账

---
+++ pages/index/transfer3.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="container">
+		<view class="head">
+			<view v-if="res == 1" class="h1">
+				<view class="h11">
+					<image src="../../static/index/成功.png"></image>
+				</view>
+				<view class="h12">
+					<p>转账成功</p>
+				</view>
+			</view>
+			<view v-else class="h1">
+				<view class="h11">
+					<image src="../../static/index/失败.png"></image>
+				</view>
+				<view class="h12">
+					<p style="color: red;">转账失败</p>
+				</view>
+			</view>
+			<view class="h2">
+				<view class="h21">
+					<p>转账记录</p>
+				</view>
+				|
+				<view class="h21">
+					<p>继续转账</p>
+				</view>
+			</view>
+		</view>
+		<view class="body">
+			<button>完成</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				res: 0
+			}
+		},
+		onLoad: function(option) {
+			this.res = option.res
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		.head {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			margin: 200rpx 0;
+
+			.h1 {
+				display: flex;
+				flex-direction: column;
+
+				.h11 {
+					image {
+						width: 150rpx;
+						height: 150rpx;
+					}
+
+					margin-bottom: 20rpx;
+				}
+
+				.h12 {
+					text-align: center;
+					font-size: 40rpx;
+					font-weight: bold;
+				}
+			}
+
+			.h2 {
+				margin-top: 10rpx;
+				display: flex;
+				color: #00d8bb;
+
+				.h21 {
+					margin: 0 10rpx;
+				}
+			}
+		}
+
+		.body {
+			width: 70%;
+
+			button {
+				background-color: #ffbf00;
+				color: #ffffff;
+			}
+		}
+	}
+</style>
